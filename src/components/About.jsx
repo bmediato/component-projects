0bdaa17279f315e5041a8de6c2893d07ac3ab404
Beatriz <beatriz.mediatto2@gmail.com>
2023-10-11 18:47:48 -0300

    components - projects and about-me

---
+++ src/components/About.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import './style/About.css'
+import technologies from "./technologies";
+
+function About() {
+  return(
+    <div>
+      <h1>Sobre mim</h1>
+      <div className="about-container">
+        <section className="about-me">
+      <h2>Sobre</h2>
+      <p>
+        <strong>Desenvolvedor Web Full-Stack</strong> em formação pela Trybe,
+        apaixonado por tecnologia e em busca de minha primeira oportunidade no mercado.<br />
+        <strong>Tenho me especializado em tecnologias como JavaScript, React, Redux, CSS e HTML,
+        além de ter experiência em Jest e RTL para testes.</strong><br /> Ao longo dos meus estudos,
+        tenho buscado aprimorar minhas habilidades nessas áreas.<br />
+        <strong>Estou animado para colaborar em projetos desafiadores e
+        continuar aprendendo</strong> com profissionais experientes,
+        visando meu crescimento profissional no campo do desenvolvimento web.
+      </p>
+        </section>
+        <div className="technologies">
+        {
+          technologies.map(({technology, image}) => (
+            <div>
+              <img src={image} alt={technology} width='65px' />
+              <p>{technology}</p>
+            </div>
+          ))
+        }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/technologies.js
@@ -0,0 +1,41 @@
+import Js from './images/javascript.png'
+import node from './images/node.png'
+import react from './images/react.png'
+import mocha from './images/mocha.png'
+import rtl from './images/rtl.png'
+import sinon from './images/sinon.png'
+import css from './images/css.png'
+import html from './images/html.png'
+import docker from './images/docker.png'
+import sequelize from './images/sequelize.png'
+import redux from './images/redux.png'
+import mysql from './images/mysqll.png'
+import ts from './images/ts.png'
+import chai from './images/chai.png'
+import git from './images/git.png'
+import api from  './images/api.png'
+
+
+
+const technologies = [
+  { technology: 'JavaScript', image: Js},
+  { technology: 'TypeScript', image: ts},
+  { technology: 'HTML', image: html},
+  { technology: 'CSS', image: css},
+  { technology: 'React', image: react},
+  { technology: 'RTL', image: rtl},
+  { technology: 'Redux', image: redux},
+  { technology: 'ContextAPi', image: react},
+  { technology: 'Git', image: git},
+  { technology: 'NodeJs', image: node},
+  { technology: 'Express.js', image: Js},
+  { technology: 'Docker', image: docker},
+  { technology: 'MySql', image: mysql},
+  { technology: 'Sequelize', image: sequelize},
+  { technology: 'Sinon', image: sinon},
+  { technology: 'Mocha', image: mocha},
+  { technology: 'Chai', image: chai},
+  { technology: 'Api Rest', image: api},
+];
+
+export default technologies
