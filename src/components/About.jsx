79ba653afd80025a182ceffdce711cc5a0279ef5
Beatriz <beatriz.mediatto2@gmail.com>
2023-10-16 17:41:54 -0300

    Header e Footer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,13 +20,13 @@ function About() {
       <p>
       <strong>Sou um desenvolvedor web full-stack</strong> em formação na Trybe,
       em busca de minha primeira oportunidade no mercado.<br />
-      <strong>Tenho me especializado em tecnologias tanto para o front-end,
+      <strong> Tenho me especializado em tecnologias tanto para o front-end,
       como JavaScript, React, Redux, Context API, CSS e HTML,
       quanto para o back-end, como TypeScript, Node.js, Express.js,
       MySQL, Sequelize e Docker. Possuo experiência em Jest e RTL para testes,
       bem como Sinon, Mocha e Chai para testes de back-end.</strong><br />
-      Durante meus estudos, tenho trabalhado para aprimorar minhas habilidades nessas áreas.
-      <strong>Estou ansioso com a possibilidade de colaborar em projetos desafiadores
+       Durante meus estudos, tenho trabalhado para aprimorar minhas habilidades nessas áreas.
+      <strong> Estou ansioso com a possibilidade de colaborar em projetos desafiadores
       e continuar aprendendo com profissionais experientes.</strong>
       Meu objetivo é crescer profissionalmente no campo do <strong>desenvolvimento web.</strong>
       </p>
